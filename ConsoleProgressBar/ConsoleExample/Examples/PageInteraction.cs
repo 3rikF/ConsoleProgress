d2354c7e76731ea2b2067330cb256439bb47d585
3rikF <70095154+3rikF@users.noreply.github.com>
2024-08-23 23:14:20 +0200

    Neues Projekt und Beispiele hinzugefügt
    Die Visual Studio Solution-Datei `ConsoleProgressBar.sln` wurde aktualisiert, um ein neues Projekt `ConsoleExample` hinzuzufügen. Dies umfasst die Konfigurationen für Debug- und Release-Builds.
    
    In `ConsoleProgressColors.cs` und `ConsoleProgressStyle.cs` wurden den vordefinierten Farben und Stilen Namen hinzugefügt.
    
    Eine neue Projektdatei `ConsoleExample.csproj` wurde erstellt, die das Ziel-Framework `net8.0` verwendet und eine Referenz auf das `ConsoleProgressBar`-Projekt enthält.
    
    Folgende Dateien wurden hinzugefügt, um verschiedene Beispiele für Fortschrittsbalken zu demonstrieren:
    - `Program.cs`: Startet die Konsolenanwendung und zeigt verschiedene Beispiele.
    - `Header.cs`: Zeigt eine Kopfzeile an.
    - `CustomStylesExample.cs`: Demonstriert benutzerdefinierte Stile.
    - `DefaultColorsExample.cs`: Demonstriert vordefinierte Farben.
    - `DefaultStylesExample.cs`: Demonstriert vordefinierte Stile.
    - `ExampleBase.cs`: Basisklasse für die Beispielklassen.
    - `RandomColorsandStyleExample.cs`: Demonstriert zufällige Kombinationen von Farben und Stilen.
    - `PageInteraction.cs`: Ermöglicht die Navigation zwischen Beispielseiten.

---
+++ ConsoleProgressBar/ConsoleExample/Examples/PageInteraction.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleExample.Examples
+{
+	internal sealed class PageInteraction(int NumPages, int startPage=0)
+	{
+		private int _curPage = int.Clamp(startPage, 0, NumPages - 1);
+
+		public event Action<int> ShowPage		= iPage => Console.WriteLine($"Page {iPage + 1} of {NumPages}");
+		public event Func<int, int> NextPage	= iPage => Math.Min(++iPage, NumPages - 1);
+		public event Func<int, int> PrevPage	= iPage => Math.Max(--iPage, 0);
+		public event Action Exit				= () => Environment.Exit(0);
+
+		public event Func<int, bool> HasNextPage = curPage => curPage < NumPages - 1;
+		public event Func<int, bool> HasPrevPage = curPage => curPage > 0;
+
+
+		public void Start()
+		{
+			while (true)
+			{
+				bool hasPrevPage = HasPrevPage(_curPage);
+				bool hasNextPage = HasNextPage(_curPage);
+
+				ShowPage(_curPage);
+
+				Console.Write("[ R: reload current page ");
+
+				if (hasPrevPage)
+					Console.Write("| P: previous page ");
+
+				if (hasNextPage)
+					Console.Write("| N: next page ");
+
+				Console.WriteLine(" | E to exit ]");
+
+				while(true)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+
+					if (key.Key == ConsoleKey.R)
+						break;
+
+					else if (key.Key == ConsoleKey.N && hasNextPage)
+					{
+						_curPage = NextPage(_curPage);
+						break;
+					}
+
+					else if (key.Key == ConsoleKey.P && hasPrevPage)
+					{
+						_curPage = PrevPage(_curPage);
+						break;
+					}
+
+					else if (key.Key == ConsoleKey.E)
+						Exit();
+
+					// delete last invalid char from console
+					Console.Write("\b \b");
+				}
+			}
+		}
+	}
+}
---
+++ ConsoleProgressBar/ConsoleExample/Examples/RandomColorsandStyleExample.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleProgressBar;
+
+namespace ConsoleExample.Examples;
+
+internal sealed class RandomColorsandStyleExample(string name, ExampleBase header) : ExampleBase(name, header)
+{
+	private const int NUM_BARS = 10;
+
+	private static readonly Random _random = new(DateTime.Now.Microsecond);
+
+	private readonly ConsoleProgressStyle[] _defaultStyles	= GetDefaultStyles();
+	private readonly ConsoleProgressColors[] _defaultColors	= GetDefaultColors();
+
+	private static T RandomElement<T>(T[] array)
+		=> array[_random.Next(array.Length)];
+
+	protected override void ImplementExample()
+	{
+		Console.WriteLine(Name);
+		Console.WriteLine("------------------------------------------------------------");
+		Console.WriteLine();
+
+		IEnumerable<ConsoleProgressHandler<int>> bars = Enumerable.Range(0, 10)
+			.Select(_ => (style:RandomElement(_defaultStyles), colors:RandomElement(_defaultColors)))
+			.Select(it => ExampleCollection.ConsoleProgress($"[{it.style.Name} x {it.colors.Name}]".PadRight(30))
+				.WithStyle(it.style)
+				.WithColors(it.colors)
+				.WithMaxBarLength(60)
+			);
+
+		//--- sequential ------------------------------------------------------
+		//foreach (ConsoleProgressHandler<int> bar in bars)
+		//{
+		//	foreach (int i in bar)
+		//		DelayStep(i);
+		//}
+
+		//--- parallel --------------------------------------------------------
+		RunParallel(bars, ExampleCollection.Length);
+	}
+}
