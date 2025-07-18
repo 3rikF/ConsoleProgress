845fe95fe710e2cc83bbe38f013f8c08471c11ae
3rikF <70095154+3rikF@users.noreply.github.com>
2025-07-18 23:43:51 +0200

    This commit introduces new tests in `ProgressProxyExtensionsTests.cs` and `TestConsoleTests.cs` to validate the behavior of the `ConsoleProgressBar` class and its interaction with `ConsoleTest` and `ConsoleReal`.

---
+++ ConsoleProgressBar/ConsoleProgressBarTests/ProgressProxyExtensionsTests.cs
@@ -0,0 +1,66 @@
+using System.Reflection;
+
+using ConsoleProgressBar;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+namespace ConsoleProgressBarTests;
+
+//-----------------------------------------------------------------------------------------------------------------------------------------
+public sealed class ProgressProxyExtensionsTests
+{
+	//-----------------------------------------------------------------------------------------------------------------
+	#region Test Methods
+
+	[Fact]
+	public void WithDebugMode_ChangesConsoleToConsoleDebug()
+	{
+		//--- ARRANGE ---------------------------------------------------------
+		byte[] testData = [1, 2, 3, 4, 5];
+		ProgressProxy<byte> sut	= new ConsoleProgressHandler<byte>(testData);
+
+		//--- ACT -------------------------------------------------------------
+		sut = sut.WithTestMode();
+
+		//--- ASSERT ----------------------------------------------------------
+		_ = Assert.IsType<ConsoleTest>(sut.Console);
+	}
+
+	[Fact]
+	public void DefaultConsoleIsConsoleReal()
+	{
+		//--- ARRANGE ---------------------------------------------------------
+		byte[] testData = [1, 2, 3, 4, 5];
+
+		//--- ACT -------------------------------------------------------------
+		ProgressProxy<byte> sut	= new ConsoleProgressHandler<byte>(testData);
+
+		//--- ASSERT ----------------------------------------------------------
+		_ = Assert.IsType<ConsoleReal>(sut.Console);
+	}
+
+	[Fact]
+	public void ConsoleProgressHandler_SingleCollectionConstructor_SetsAppropriateStrings()
+	{
+		//--- ARRANGE ---------------------------------------------------------
+		byte[] testData	 = [1, 2, 3, 4, 5];
+
+		//--- ACT -------------------------------------------------------------
+		ConsoleProgressHandler<byte> sut = new(testData);
+
+		//--- ASSERT ----------------------------------------------------------
+		// Using reflection to access the protected properties
+		PropertyInfo? actionDescProperty = typeof(ProgressProxy<byte>)
+			.GetProperty("ActionDesc", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		PropertyInfo? itemDescProperty = typeof(ProgressProxy<byte>)
+			.GetProperty("ItemDesc", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		string? actionDesc	= actionDescProperty?.GetValue(sut) as string;
+		string? itemDesc	= itemDescProperty?.GetValue(sut) as string;
+
+		Assert.Null(actionDesc);
+		Assert.Null(itemDesc);
+	}
+
+	#endregion Test Methods
+}
